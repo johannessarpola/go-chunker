ac7ad562d06a848c9ec2e8665b17bc6b522e3605
Johannes Sarpola <johannes.sarpola@qalmari.fi>
2024-12-19 10:41:19 +0200

    add some basic flags

--- cmd/chunk/main.go
+++ cmd/chunk/main.go
@@ -1,1 +1,48 @@
-package chunk
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/urfave/cli/v3"
+	"log"
+	"os"
+)
+
+func main() {
+	var input, output string
+	var size int64
+
+	cmd := &cli.Command{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "input",
+				Usage:       "input file for chunking",
+				Aliases:     []string{"i"},
+				Destination: &input,
+			},
+			&cli.StringFlag{
+				Name:        "output",
+				Usage:       "ouptut directory for chunks",
+				Value:       "out",
+				Aliases:     []string{"o"},
+				Destination: &output,
+			},
+			&cli.IntFlag{
+				Name:        "size",
+				Usage:       "size of chunk",
+				Aliases:     []string{"s"},
+				Destination: &size,
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Printf("input: %s\n", input)
+			fmt.Printf("output: %s\n", output)
+			fmt.Printf("size: %d\n", size)
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/johannessarpola/go-chunker
 
 go 1.23.0
+
+require github.com/urfave/cli/v3 v3.0.0-beta1 // indirect
