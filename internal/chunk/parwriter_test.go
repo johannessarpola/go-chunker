fd6481e868442bde187d3cae1658116ee1debf41
Johannes Sarpola <johannes.sarpola@gmail.com>
2024-12-31 15:19:04 +0200

    mkdir (fix)

--- internal/chunk/parwriter_test.go
+++ internal/chunk/parwriter_test.go
@@ -13,7 +13,7 @@ func TestParWriterFile(t *testing.T) {
 	source, err := NewFileSource(td)
 	require.NoError(t, err)
 
-	err = os.Mkdir("out")
+	err = os.Mkdir("out", os.ModeDir)
 	require.NoError(t, err)
 
 	o := Output{
@@ -37,7 +37,7 @@ func TestParWriterDirectory(t *testing.T) {
 	source, err := NewDirectorySource(td)
 	require.NoError(t, err)
 
-	err = os.Mkdir("out")
+	err = os.Mkdir("out", os.ModeDir)
 	require.NoError(t, err)
 
 	o := Output{
